1140ecbc168fd24feba2e8e86d29fad8d6b36989
elvynmejia <elvynmejia@gmail.com>
2015-08-17 15:56:05 -0700

    Order model to capture order details, validations, error reporting, order feed for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :line_items
-  resources :carts
+  	resources :orders
+  	resources :line_items
+  	resources :carts
+
 	get 'store/index'
-  	resources :products
+  	resources :products do 
+  		get :who_bought, on: :member
+  	end 
+
  	root 'store#index', as: 'store'
 end
