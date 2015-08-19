74adaf1bf86b0b358a5eff2c4b14cdfb9353e96e
elvynmejia <elvynmejia@gmail.com>
2015-08-19 14:12:03 -0700

    Create mailer to send order and shipping confirmation, add sendgrid and fuctional/integration test

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -38,7 +38,8 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-    
+        OrderNotifier.received(@order).deliver_now
+
         format.html { redirect_to store_url, notice: 
           'Thank you for your order.' }
         format.json { render action: 'show', status: :created,
